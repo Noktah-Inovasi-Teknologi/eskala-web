c638202a56425c1de73c81e557359482dc7f8c79
Bagas Yadher Bima Nugraha Ainur Rofiq Hidayat <bagas.ybnarh@gmail.com>
2025-11-25 17:42:04 +0700

    Added Tailwindcss

---
+++ app/pages/index.vue
@@ -0,0 +1,9 @@
+<script languange="ts" setup>
+
+</script>
+
+<template>
+  <div class="text-red-500">
+    Maklo Gimang
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
+  css: ["./app/assets/css/main.css"],
   devtools: { enabled: true },
   hub: {
     database: true,
   },
-  modules: [
-    '@nuxt/content',
-    '@nuxt/ui',
-    '@nuxthub/core'
-  ]
-})
+  modules: ["@nuxt/content", "@nuxt/ui", "@nuxthub/core"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
